1f91e039ade84f53e2e7db7501a41011254264b7
user <athimbityana@gmail com>
2022-06-15 23:35:21 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Shops to visit:");
+
+        System.out.println("\n\t\tAckermans");
+        System.out.println("\n\t\tGame");
+        System.out.println("\n\t\tDionwired");
+        System.out.println("\n\t\tH&H");
+    }
+}
